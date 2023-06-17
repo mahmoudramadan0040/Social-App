8d4fb6f691e3d05f9ca52de30527cb420d45c351
KarimMaged <karim.maged2020@yahoo.com>
2023-06-17 16:28:01 +0300

    import models into the init for login_manager to work properly

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
+
 #create an instance of flask
 app = Flask(__name__)
 # the secret key
@@ -12,13 +13,13 @@ app.config['SECRET_KEY'] = 'W9MZ9FbVYKXHPDVqpvtkl1i16dnU3MWr'
 #setting up the bcrypt
 bcrypt = Bcrypt(app)
 
-# setting up LoginManager
-login_manager = LoginManager(app)
+
 
 #setting up the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:18501900@localhost:5432/flask_db'
 db = SQLAlchemy(app)
 
-
+# setting up LoginManager
+login_manager = LoginManager(app)
 # importing the routes
-from flask_app import routes
+from flask_app import routes,models
