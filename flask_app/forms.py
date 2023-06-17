53214eb97edb9e0e7da71b76a35f826e5e16b64b
mahmoudramadan0020 <mahmoudramadan0040@gmail.com>
2023-06-17 20:37:10 +0300

    crud post

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -1,3 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,DateField,TextAreaField
 from wtforms.validators import DataRequired,length,Email,EqualTo
+class PostFrom(FlaskForm):
+    title = StringField(
+        'title',
+        validators=[
+            DataRequired()
+        ]
+    )
+    content = TextAreaField(
+        'content',
+        validators=[
+            DataRequired()
+        ]
+    )
+    date_posted =DateField(
+        'date_posted'
+    )
+    submit = SubmitField(
+        'Save Post'
+    )
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template,redirect,request,url_for
 from flask_app import bcrypt,login_manager,app,db
 from flask_login import current_user,login_required,login_user,logout_user
 from flask_app.models import User,Post
+from flask_app.forms import PostFrom 
 from PIL import Image
 from io import BytesIO
 #home endpoint
@@ -49,4 +50,54 @@ def profile():
 def friend_requests():
     return "friend requests"
 
+
+@app.route('/post/create',methods=['GET','POST'])
+def create_post():
+    form= PostFrom()
+    if form.validate_on_submit():
+        with app.app_context():
+            new_post = Post(
+                title=form.title.data,
+                content =form.content.data,
+                date_posted = form.date_posted.data,
+                user_id=1
+            )
+            db.session.add(new_post)
+            db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('posts/create.html',data={'form':form})
+            
+@app.route('/posts',methods=['GET'])
+def list_post():
+    with app.app_context():
+        posts =Post.query.all()
+        print(posts)
+    return render_template('posts/list.html',posts=posts)
+    
+@app.route('/post/<int:id>')
+def get_post(id):
+    with app.app_content():
+        post =Post.query.filter_by(id=id).first()
+        if post:
+            return render_template('',post=post)
+
+@app.route('/post/<int:id>/update',methods=['GET','POST'])  
+def update_post(id):
+    post= Post.query.filter_by(id=id).first()
+    if post:
+        form= PostFrom(obj=post)
+        if form.validate_on_submit():
+            form.populate_obj(post)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('home'))
+        return render_template('posts/create.html', data={'form':form})
+    else:
+        return "Post not exists to update !"
+
+# @app.route('/post/<int:id>/delete',methods=['GET','POST'])
+# def delete_post(id):
+#     post =Post.filter_by(id=id).first()
+#     if request.method == 'POST' :
+        
 
